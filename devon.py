3178ba5b7eee028ce61f6c809727e5588d3d628a
Devon Wasson <dtw006@bucknell.edu>
2017-02-05 16:02:42 -0500

    mostly fixed raw input

--- devon.py
+++ devon.py
@@ -10,10 +10,10 @@ import heapq
 # g = generals.Generals('your userid', 'your username', 'ffa')
 
 # private game
-roomID = raw_input("Please enter the room id: ")
+#roomID = raw_input("Please enter the room id: ")
 #print(roomID)
 
-g = generals.Generals('ry0FVyx_g', 'frank', 'private', roomID)
+#g = generals.Generals('ry0FVyx_g', 'frank', 'private', roomID)
 
 # 2v2 game
 # g = generals.Generals('your userid', 'your username', 'team')
@@ -41,17 +41,18 @@ class Bot:
 		self.roomID = ""       # the id of current game
 		self.nCols = 0         # number of columns in the map
 		self.nRows = 0         # number of rows in the map
+		self.game = None
 
 	def startGame(self,game="private"):
 		if game == "1v1":
-			g = generals.Generals('ry0FVyx_g', 'frank', '1v1')
+			self.game = generals.Generals('ry0FVyx_g', 'frank', '1v1')
 		elif game == "ffa":
-			g = generals.Generals('ry0FVyx_g', 'frank', 'ffa')
+			self.game = generals.Generals('ry0FVyx_g', 'frank', 'ffa')
 		elif game == "private":
 			self.roomID = raw_input("Please enter the room id: ")
-			g = generals.Generals('ry0FVyx_g', 'frank', 'private', self.roomID)
+			self.game = generals.Generals('ry0FVyx_g', 'frank', 'private', self.roomID)
 		elif game == "2v2":
-			g = generals.Generals('ry0FVyx_g', 'frank', 'team')
+			self.game = generals.Generals('ry0FVyx_g', 'frank', 'team')
 		else:
 			print("Please input one of the following game types: ")
 			print("1v1, ffa, private, 2v2")
@@ -111,7 +112,7 @@ class Bot:
 
 
 	def createMap(self, update):
-		'''Craetes the initial Map - a 2D array with a 
+		'''Craetes the initial Map - a 2D array with a
 		Tile object in each index'''
 		#print("creating map!")
 		self.nCols = update['cols']
@@ -147,21 +148,36 @@ class Bot:
 
 
 frank = Bot()
+frank.startGame()
 
 madeMap = False
 
+stackX = 0
+stackY = 0
 
-for update in g.get_updates():
-	frank.printMap(update)
+for update in frank.game.get_updates():
+#	frank.printMap(update)
 	if madeMap == False:
 		frank.initializeMap(update)
 		madeMap = True
-	# get position of your general
-	pi = update['player_index']
+		pi = update['player_index']
+		crownY, crownX = update['generals'][pi]
+		print("Crown is at [x,y]: [" + str(crownX) + "," + str(crownY) + "]")
+	
+	print("Crown: " + str(update['tile_grid'][crownY][crownX]))
+	print("Up: " + str(update['tile_grid'][crownY - 1][crownX]))
+	print("Down: " + str(update['tile_grid'][crownY + 1][crownX]))
+	print("Left: " + str(update['tile_grid'][crownY][crownX - 1]))
+	print("Right: " + str(update['tile_grid'][crownY][crownX + 1]))
+
+	
+
 	y, x = update['generals'][pi]
+	print("x: " + str(x))
+	print("y: " + str(y))
 	# move units from general to arbitrary square
+
 	for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
 		if (0 <= y+dy < update['rows'] and 0 <= x+dx < update['cols'] and update['tile_grid'][y+dy][x+dx] == 0):
-			g.move(y, x, y+dy, x+dx)
+			frank.game.move(y, x, y+dy, x+dx)
 			break
-	
